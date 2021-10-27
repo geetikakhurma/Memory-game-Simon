f5d8b16e1fae7265e7664c569c23a681b6659108
geetika <geetikakhurma99@gmail.com>
2021-10-27 23:35:20 +0530

    first commit

---
+++ src/components/ColorCard.js
@@ -0,0 +1,9 @@
+   
+export default function ColorCard({ color, onClick, flash }) {
+    return (
+        <div
+        onClick={onClick}
+        className={`colorCard ${color} ${flash ? "flash" : ""}`}
+      ></div>
+    )
+}
